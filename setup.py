b3aeba4211293335da3f7ff9e2e2d0d661aef769
SubaashNair <valiban12@gmail.com>
2025-04-02 11:42:42 +0800

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="outlier-cleaner",
-    version="0.1.1",
+    version="0.1.2",
     author="Subashan Annair",
-    author_email="your.email@example.com",  # Replace with your email
+    author_email="subaashnair12@gmail.com",  # Replace with your email
     description="A Python package for detecting and removing outliers in data using various statistical methods",
     long_description=long_description,
     long_description_content_type="text/markdown",
